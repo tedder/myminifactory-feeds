d3b3f7a1fd90ca9c15797d955bc2a2bd1db802ab
Ted Timmons <ted@perljam.net>
2019-05-30 13:17:27 -0700

    update

--- publish.py
+++ publish.py
@@ -6,6 +6,7 @@ import boto3
 import configparser
 import json
 import mimetypes
+import os
 import requests
 
 s3 = boto3.client('s3')
@@ -43,9 +44,10 @@ def create_item(item):
   for i in item['images']:
     if i['is_primary']:
       #i['original']['url']
+      #print(i.keys())
       retitem['image'] = i['large']['url']
       # also add to the bottom of the content; at least Newsblur doesn't use the image field.
-      retitem['content_html'] += '<br clear="all" /><img src="{}">'.format(i['large']['url'])
+      retitem['content_html'] = '<img src="{}"><br clear="all" />{}'.format(i['thumbnail']['url'], retitem['content_html'])
     else:
       retitem['attachments'].append({
         'mime_type': mimetypes.guess_type(i['large']['url'])[0],
@@ -79,14 +81,17 @@ def makefeed(params_override, page_url, s3_key, feed_title, api_key):
   }
   c = 0
   for i in ret['items']:
+    if any(x in i['name'].lower() for x in ['bauble', 'ornament']): continue
     jfeed['items'].append(create_item(i))
     c += 1
     if c > 40: break
   #print(json.dumps(jfeed, indent=2))
   upload(jfeed, s3_key)
 
+# sniff our script dir, useful since we're a cron
+scriptdir = os.path.dirname(os.path.realpath(__file__))
 conf = configparser.ConfigParser()
-conf.read('creds.ini')
+conf.read(os.path.join(scriptdir, 'creds.ini'))
 api_key = conf['creds']['api_key']
 
 makefeed(
